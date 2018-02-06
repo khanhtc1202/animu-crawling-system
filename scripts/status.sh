5ac6e9f0c874a3df0e7ed4343b07f30ae90a6f31
khanhtc1202 <khanhtc1202@gmail.com>
2018-02-06 18:37:06 +0000

    update status script

--- scripts/status.sh
+++ scripts/status.sh
@@ -5,6 +5,26 @@
 # script check system status
 #
 
+echo "--------- Process Status ---------"
+
 ps aux | grep -E "[n]odemon|[p]ython|[a]ria2"
 
+echo "------- Hard Disk Capacity -------"
+
 df -h | grep none
+
+echo "----------------------------------------"
+
+echo "Time:"
+
+hour=$(date +%H)
+StartDate=$(date -u -d "$NOW" +"%s")
+FinalDate_1=$(date -u -d "05:22:15" +"%s")
+FinalDate_2=$(date -u -d "17:22:15" +"%s")
+if [ "$hour" -gt 17 ]
+    then echo "Time to the next running turn of crawling process is $(date -u -d "0 $FinalDate_1 sec - $StartDate sec" +"%H:%M:%S")"
+elif [ "$hour" -lt 5 ]
+    then echo "Time to the next running turn of crawling process is $(date -u -d "0 $FinalDate_1 sec - $StartDate sec" +"%H:%M:%S")"
+else
+    echo "Time to the next running turn of crawling process is $(date -u -d "0 $FinalDate_2 sec - $StartDate sec" +"%H:%M:%S")"
+fi
