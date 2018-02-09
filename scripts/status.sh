b4c3c0e8f27882b575e39615bf0feeb23556474a
khanhtc1202 <khanhtc1202@gmail.com>
2018-02-09 10:02:57 +0000

    update status script avoid counting not media files

--- scripts/status.sh
+++ scripts/status.sh
@@ -15,7 +15,7 @@ df -h | grep Filesystem -A 1
 
 echo "----- Last downloaded status -----"
 
-dwFiles=$(find ../media/* -ctime -0.5 | wc -l)
+dwFiles=$(find ../media/* -ctime -0.5 | grep -E "mp4|mkv" | wc -l)
 echo "Downloaded $dwFiles file(s) in the last time crawling service run"
 
 echo "----------------------------------------"
@@ -24,8 +24,8 @@ echo "Time:" $(date '+%Y/%m/%d---%H:%M:%S')
 
 hour=$(date +%H)
 StartDate=$(date -u -d "$(date +%H:%M:%S)" +"%s")
-FinalDate_1=$(date -u -d "05:22:15" +"%s")
-FinalDate_2=$(date -u -d "17:22:15" +"%s")
+FinalDate_1=$(date -u -d "06:04:28" +"%s")
+FinalDate_2=$(date -u -d "18:04:28" +"%s")
 if [ "$hour" -gt 17 ]
     then echo "Time to the next running turn of crawling process is $(date -u -d "0 $FinalDate_1 sec - $StartDate sec" +"%H:%M:%S")"
 elif [ "$hour" -lt 5 ]
