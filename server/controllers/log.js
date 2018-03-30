1948a1b8e9f6f8c65206b4bb30a38876bb8758ff
khanhtc1202 <khanhtc1202@gmail.com>
2018-03-30 08:11:52 +0000

    kill tail process on close connection

--- server/controllers/log.js
+++ server/controllers/log.js
@@ -5,6 +5,12 @@ module.exports = function(config, utils) {
     obj.show = function(req, res, next) {
         res.header('Content-Type','text/html;charset=utf-8');
         var tail = spawn('tail', ['-f', config.app.logPath]);
+        res.on('close', function(){
+            console.log("Response ended");
+            tail.kill("SIGINT");
+        }).on('finish', function() {
+            console.log("Response finished successfully");
+        });
         tail.stdout.pipe(res);
     };
     
